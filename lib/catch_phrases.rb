274bf40557de95ff7802e158e7cd6c93749be3b1
George Chen <georgechen1338@gmail.com>
2020-03-17 23:09:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,4 @@
-def mario(phrase)
-  describe "mario" do
-    it "puts out 'It's-a me, Mario!'" do
-      expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-    end
-  end
+def mario(phrase = 'Mario')
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
